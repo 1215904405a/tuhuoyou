5abb763ce9ee55a569fb63b6e606453cddb780c4
wangyong <wangyong@qding.me>
2018-01-18 17:08:32 +0800

    rollup  test

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,18 @@
 export default {
-  input: 'src/entry.js',
+  input: 'src/app.js',
   output: [{
-    file: 'dist/rollup_bundle.js',
+    file: 'dist/app_bundle.js',
     format: 'iife', //Type of output (amd, cjs, es, iife, umd)
+    // amd - 输出成AMD模块规则， RequireJS可以用
+    // cjs - CommonJS规则， 适合Node， Browserify， Webpack 等
+    // es - 默认值， 不改变代码
+    // iife - 输出自执行函数， 最适合导入html中的script标签， 且代码更小
+    // umd - 通用模式， amd,
+    // cjs,
+    // iife都能用
   }]
 }
+
+//提示：
+//rollup main.js --format iife --output bundle.js
+//
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ const webpack = require('webpack');
 
 module.exports = {
   // devtool: "source-map",
-  entry: "./src/entry.js",
+  entry: "./src/app.js",
   output: {
     path: __dirname + "/dist",
-    filename: "webpack.bundle3.js"
+    filename: "webpack.app.js"
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
-    new webpack.optimize.ModuleConcatenationPlugin()
+    // new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
+    // new webpack.optimize.ModuleConcatenationPlugin(), //作用域提升（模块代码放一起）
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('development') })
   ]
 }
