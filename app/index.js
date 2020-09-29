203810a2cd954e1cd0e1e7a5399ab5ee16ed78cb
0003414 <wangyong@mininglamp.com>
2020-09-29 10:47:23 +0800

    fix: bug

--- app/index.js
+++ app/index.js
@@ -21,7 +21,7 @@ const app = module.exports = new Koa();
 const port = config.port;
 
 // 自己生成ssl
-app.use(sslify())
+// app.use(sslify())
 
 // 加载ejs模板
 app.use(views(__dirname + '/template', { extension: 'ejs' }));
@@ -65,35 +65,35 @@ app.use(rt.routes()) // 启动路由
 app.use(rt2.routes())
 app.use(router.allowedMethods())
 
-const options = {
-    /*
-        openssl genrsa -out private_key.pem 1024
-        参数解释：genrsa -- 用RSA算法， 
-        private_key.pem导出的私钥名称
-    */
-    // key: fs.readFileSync('./private_key.pem'),  // 私钥文件路径 自己命令生成
-    key: fs.readFileSync('./certificate/4564894_www.tuhuoyou.com.key'), // 阿里云购买
+// const options = {
+//     /*
+//         openssl genrsa -out private_key.pem 1024
+//         参数解释：genrsa -- 用RSA算法， 
+//         private_key.pem导出的私钥名称
+//     */
+//     // key: fs.readFileSync('./private_key.pem'),  // 私钥文件路径 自己命令生成
+//     key: fs.readFileSync('./certificate/4564894_www.tuhuoyou.com.key'), // 阿里云购买
 
-    /*
-        openssl req -new -out ca-req.csr -key private_key.pem
-        参数解释：ca-req.csr -- 证书请求名称， 
-        private_key.pem前面生成的传输私钥的名称
-        openssl x509 -req -in ca-req.csr -out ca-cert.pem -signkey private_key.pem -days 3650
-    */
-    // cert: fs.readFileSync('./.pem')  // 证书文件路径 自己生成
-    cert: fs.readFileSync('./certificate/4564894_www.tuhuoyou.com.pem') // 阿里云购买
-};
+//     /*
+//         openssl req -new -out ca-req.csr -key private_key.pem
+//         参数解释：ca-req.csr -- 证书请求名称， 
+//         private_key.pem前面生成的传输私钥的名称
+//         openssl x509 -req -in ca-req.csr -out ca-cert.pem -signkey private_key.pem -days 3650
+//     */
+//     // cert: fs.readFileSync('./.pem')  // 证书文件路径 自己生成
+//     cert: fs.readFileSync('./certificate/4564894_www.tuhuoyou.com.pem') // 阿里云购买
+// };
 
-https.createServer(options, app.callback()).listen(port, () => {
-    const decorator = new Array(40).fill('*').join('');
-    console.info(colors.cyan(decorator));
-    console.info(colors.green(`成功:服务已经启动, http://localhost:${port}`));
-    console.info(colors.cyan(decorator));
-});
-
-// app.listen(port, function () {
-//     let decorator = new Array(40).fill('*').join('');
+// https.createServer(options, app.callback()).listen(port, () => {
+//     const decorator = new Array(40).fill('*').join('');
 //     console.info(colors.cyan(decorator));
 //     console.info(colors.green(`成功:服务已经启动, http://localhost:${port}`));
 //     console.info(colors.cyan(decorator));
 // });
+
+app.listen(port, function () {
+    let decorator = new Array(40).fill('*').join('');
+    console.info(colors.cyan(decorator));
+    console.info(colors.green(`成功:服务已经启动, http://localhost:${port}`));
+    console.info(colors.cyan(decorator));
+});
