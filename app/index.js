03080015c6c4f6915ba2f06ac1866cb2fb697b83
wangyong <wangyong@mininglamp.com>
2021-10-19 11:29:49 +0800

    update: key update

--- app/index.js
+++ app/index.js
@@ -72,7 +72,8 @@ const options = {
         private_key.pem导出的私钥名称
     */
     // key: fs.readFileSync('./private_key.pem'),  // 私钥文件路径 自己命令生成
-    key: fs.readFileSync('./certificate/4564894_www.tuhuoyou.com.key'), // 阿里云购买
+    // key: fs.readFileSync('./certificate/4564894_www.tuhuoyou.com.key'), // 阿里云购买
+    key: fs.readFileSync('./certificate/6284402_www.tuhuoyou.com.key'),
 
     /*
         openssl req -new -out ca-req.csr -key private_key.pem
@@ -81,13 +82,14 @@ const options = {
         openssl x509 -req -in ca-req.csr -out ca-cert.pem -signkey private_key.pem -days 3650
     */
     // cert: fs.readFileSync('./.pem')  // 证书文件路径 自己生成
-    cert: fs.readFileSync('./certificate/4564894_www.tuhuoyou.com.pem') // 阿里云购买
+    // cert: fs.readFileSync('./certificate/4564894_www.tuhuoyou.com.pem') // 阿里云购买
+    cert: fs.readFileSync('./certificate/6284402_www.tuhuoyou.com.pem')
 };
 
 https.createServer(options, app.callback()).listen(port, () => {
     const decorator = new Array(40).fill('*').join('');
     console.info(colors.cyan(decorator));
-    console.info(colors.green(`成功:服务已经启动, http://localhost:${port}`));
+    console.info(colors.green(`成功:服务已经启动, https://localhost:${port}`));
     console.info(colors.cyan(decorator));
 });
 
